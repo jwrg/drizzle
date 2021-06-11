58bc0861011affdf386e4e1b34a8b6e245ceceac
Jim Galloway <jwrg@uvic.ca>
2021-06-10 21:36:10 -0700

    Readability edits and Timer issue fix

--- drizzle.py
+++ drizzle.py
@@ -1,10 +1,13 @@
 import time
 import logging
 from threading import Timer
-import piplates.RELAYplate as plate
+from piplates.RELAYplate import relaySTATE
+from piplates.RELAYplate import relayON
+from piplates.RELAYplate import relayOFF
 from flask import Flask
 from flask import render_template
 from flask import redirect
+from flask import url_for
 app = Flask(__name__)
 
 # Configure logging format and verbosity
@@ -13,31 +16,29 @@ app = Flask(__name__)
 NUM_ZONES = 5
 # Declare the zone number for the master switch
 MASTER_ZONE = 7
-# A list to hold timer objects
-zone_timers = []
+# A list to hold a timer object
+zone_timer = None
 
 # A function for turning a zone on
 def zoneOn(zone: int):
-    # It should be safe to turn on the master switch
-    # even when it is already switched on
-    plate.relayON(0,MASTER_ZONE)
-    # Turn on the argument zone
-    plate.relayON(0,zone)
+    state = relaySTATE(0)
+    # Turn on the argument zone only if there isn't
+    # already another zone turned on
+    if state == 0:
+        # It should be safe to turn on the master switch
+        # even when it is already switched on
+        relayON(0,MASTER_ZONE)
+        relayON(0,zone)
 
 # A function for turning a zone off (possibly early)
 def zoneOff(zone: int):
-    state = plate.relaySTATE(0)
-    # If the state is master plus argument zone
-    if state == 1 << (MASTER_ZONE - 1) + 1 << (zone - 1):
-        # Then turn off the master zone
-        plate.relayOFF(0,MASTER_ZONE)
-    # Regardless, we wish to turn off the arg zone
-    plate.relayOFF(0,zone)
+    relayOFF(0,MASTER_ZONE)
+    relayOFF(0,zone)
 
 
 @app.route('/')
 def index():
-    state = plate.relaySTATE(0)
+    state = relaySTATE(0)
     # Check each bit of state and comp a list of bools
     # which represents whether each zone is on or off
     active = [ x for x in range(0,NUM_ZONES) if (state >> x) % 2 ]
@@ -63,5 +64,5 @@ def enable(zone, time):
     zoneOn(zone)
     # Create a timer object to turn off the zone
     # after the specified number of minutes
-    zone_timers[zone] = Timer(60.0 * time, zoneOff(zone))
+    zone_timer = Timer(60.0 * time, zoneOff(zone))
     return redirect(url_for('.index'))
