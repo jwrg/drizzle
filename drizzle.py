52ee9191a4e26dbf1a9345b110995cc72612a194
Jim Galloway <jwrg@uvic.ca>
2022-08-24 22:20:10 -0700

    Get rid of some globals
    These values are intended to be set locally in config.json
    and ultimately to be edited using the edit template.

--- drizzle.py
+++ drizzle.py
@@ -23,25 +23,20 @@ basicConfig(filename='drizzle.log', level=app.config['LOG_LEVEL'])
 # the app counts and displays this as another zone.
 
 # Declare the number of sprinkler zones (not including pump)
-NUM_ZONES = 5
 # Declare the number of concurrently active zones allowed
-MAX_ZONES = 1
 # Declare the max time for which a zone may be active
-MAX_TIME = 31
 # Declare the address (board, relay) for each zone
 # NB that the interface starts counting zones at one
 # but this tuple is zero-indexed.  Relays on the
 # boards start at K1 and so the first relay is relay
 # 1, since there is no K0.  Not confusing at all.
-ZONES = (0, 1), (0, 2), (0, 3), (0, 4), (0, 5)
 # Declare the zone number for the pump switch (to
 # denote that no pump is used, give the value as None)
-PUMP_ZONE = (0, 7)
 
 # A list of references to Timer objects; this is
 # initialized as a list of None values
 # NB that zone 0 is reserved for the pump
-TIMERS = [ None for x in range(0, NUM_ZONES + 1) ]
+TIMERS = [ None for x in range(0, app.config['NUM_ZONES'] + 1) ]
 
 # An integer for holding the id of the currently running
 # sequence.  None denotes no sequence is currently active
@@ -53,49 +48,46 @@ SEQUENCE = None
 # None, which denotes that there is no active sequence
 SEQUENCE_TIMER = None
 
-# The application port on which the app listens
-APP_PORT = 8080
-
 # A function for determining which of the zone
 # relays are on, if any.  Returns a list that can
 # be passed to the index page
 def getState():
-    # Get a list of board addresses found in ZONES
-    BOARDS = list({x[0] for x in ZONES})
+    # Get a list of board addresses found in app.config['ZONES']
+    BOARDS = list({x[0] for x in app.config['ZONES']})
     BOARDS.sort()
     # Get a state for each board
     STATES = {x: relaySTATE(x) for x in BOARDS}
     # Check each state against all zones for that board
-    active = [x for x in range(0, NUM_ZONES) if (STATES[ZONES[x][0]] >> (ZONES[x][1] - 1)) % 2]
+    active = [x for x in range(0, app.config['NUM_ZONES']) if (STATES[app.config['ZONES'][x][0]] >> (app.config['ZONES'][x][1] - 1)) % 2]
     # Check the pump state, if applicable and only check
     # when no other zones are active
-    if PUMP_ZONE is not None and active == []:
-        if (STATES[PUMP_ZONE[0]] >> (PUMP_ZONE[1] - 1)) % 2:
+    if app.config['PUMP_ZONE'] is not None and active == []:
+        if (STATES[app.config['PUMP_ZONE'][0]] >> (app.config['PUMP_ZONE'][1] - 1)) % 2:
             active.append('Pump')
     return active
 
 # Functions for turning on/off the pump, when applicable
 def pumpOn():
-    if PUMP_ZONE is not None and len(getState()) < MAX_ZONES:
-        relayON(PUMP_ZONE[0], PUMP_ZONE[1])
+    if app.config['PUMP_ZONE'] is not None and len(getState()) < app.config['MAX_ZONES']:
+        relayON(app.config['PUMP_ZONE'][0], app.config['PUMP_ZONE'][1])
 def pumpOff():
-    if PUMP_ZONE is not None:
-        relayOFF(PUMP_ZONE[0], PUMP_ZONE[1])
+    if app.config['PUMP_ZONE'] is not None:
+        relayOFF(app.config['PUMP_ZONE'][0], app.config['PUMP_ZONE'][1])
 
 # A function for turning a zone on (argument not zero-indexed)
 def zoneOn(zone: int):
     # Turn on the argument zone only if there isn't
     # already another zone or sequence turned on
-    if len(getState()) < MAX_ZONES:
+    if len(getState()) < app.config['MAX_ZONES']:
         pumpOn()
-        relayON(ZONES[zone - 1][0], ZONES[zone - 1][1])
+        relayON(app.config['ZONES'][zone - 1][0], app.config['ZONES'][zone - 1][1])
 
 # A function for turning a zone off (argument not zero-indexed)
 def zoneOff(zone: int):
     # Turn off pump only when there is no active pump timer
     if len(getState()) == 1 and TIMERS[0] is None:
         pumpOff()
-    relayOFF(ZONES[zone - 1][0], ZONES[zone - 1][1])
+    relayOFF(app.config['ZONES'][zone - 1][0], app.config['ZONES'][zone - 1][1])
 
 # Functions for reading/updating the local sequences json
 # file.  Returns a dict, since the top-level type is object
@@ -142,7 +134,7 @@ def cancelSequence():
     if SEQUENCE_TIMER != None:
         SEQUENCE_TIMER.cancel()
         SEQUENCE_TIMER = None
-    for zone in ZONES:
+    for zone in app.config['ZONES']:
         relayOFF(zone[0], zone[1])
     pumpOff()
     SEQUENCE = None
@@ -212,7 +204,7 @@ def disable(zone):
 
 @app.route('/zone/enable/<int:zone>/<int:time>/')
 def enable(zone, time):
-    if time <= MAX_TIME:
+    if time <= app.config['MAX_TIME']:
         if zone == 0:
             pumpOn()
             TIMERS[0] = Timer(60.0 * time, pumpOff)
@@ -283,8 +275,8 @@ def newSequence():
             'sequence': {'0': {'zone': 1, 'minutes':1}, 'columns': ['zone','minutes']}},\
             fields = ['name', 'description', 'sequence'],\
             constrain = {'minutes': 120,\
-                'zone':[ x for x in range(1, NUM_ZONES + 1)]},\
-            num_zones=NUM_ZONES)
+                'zone':[ x for x in range(1, app.config['NUM_ZONES'] + 1)]},\
+            num_zones=app.config['NUM_ZONES'])
 
 @app.route('/sequence/edit/<int:id>/', methods=('GET', 'POST'))
 def editSequence(id):
@@ -331,9 +323,9 @@ def editSequence(id):
             subject = 'sequence',\
             item = getSequences()[str(id)],\
             fields = ['name', 'description', 'sequence'],\
-            constrain = {'minutes': 120,\
-                'zone':[ x for x in range(1, NUM_ZONES + 1)]},\
-            num_zones = NUM_ZONES)
+            constrain = {'minutes': app.config['MAX_TIME'],\
+                'zone':[ x for x in range(1, app.config['NUM_ZONES'] + 1)]},\
+            num_zones = app.config['NUM_ZONES'])
 
 @app.route('/sequence/delete/<int:id>/')
 def deleteSequence(id):
