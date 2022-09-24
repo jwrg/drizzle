40b94a7b3241fcd37d9804827376539989c1a7db
Jim Galloway <jim@origincrm.com>
2022-09-24 07:08:18 -0700

    Fix mock relay plate

--- test/plates.py
+++ test/plates.py
@@ -4,13 +4,13 @@ def relaySTATE(arg):
     return platestate
 def relayON(arg,arg2):
     global platestate
-    platestate = platestate | 1 << arg2
+    platestate = platestate | 1 << arg2 - 1
     print('__!! ON !!__'.join(['BOARD',str(arg),'RELAY',str(arg2),'ON--ON--ON']))
     print('--!STATE!--'.join((['NEWSTATE:',str(platestate)])))
     return 0
 def relayOFF(arg,arg2):
     global platestate
-    platestate = platestate & ~(1 << arg2)
+    platestate = platestate & ~(1 << arg2 - 1)
     print('__! OFF !__'.join(['BOARD',str(arg),'RELAY',str(arg2),'OFF--OFF--OFF']))
     print('--!STATE!--'.join((['NEWSTATE:',str(platestate)])))
     return 0
