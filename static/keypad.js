6ab08a567a2282cea5d4b28f7728733faef6c2ac
Jim Galloway <jim@origincrm.com>
2023-07-18 13:06:58 -0700

    Improve styling for minutes slider

--- static/keypad.js
+++ static/keypad.js
@@ -8,6 +8,6 @@ function setBubble(range, bubble) {
   const min = range.min ? range.min : 0;
   const max = range.max ? range.max : 100;
   const newVal = Number(((val - min) * 100) / (max - min));
-  bubble.innerHTML = val == 1 ? val + ' minute' : val + ' minutes';
+  bubble.innerHTML = val == 1 ? val + '&nbsp;minute' : val + '&nbsp;minutes';
   bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
 }
