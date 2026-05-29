0b4a2bde6d63e894f61ac38e507c3a50ce81b0be
Jim Galloway <jim@origincrm.com>
2026-05-29 13:41:17 -0700

    Add sequence and schedule control components to dashboard
    While still needing a good restyling, there are now
    components for visualizing the state of sequences
    and schedules, and for running and activating them
    respectively.  Keypad files have been renamed to
    conform with their use in the dashboard.

--- blueprints/dashboard/dashboard.py
+++ blueprints/dashboard/dashboard.py
@@ -1,5 +1,5 @@
 """
-Routes for configuring the app
+Routes for quick and dirty interactions with the app
 """
 from datetime import datetime
 
@@ -39,9 +39,10 @@ def index():
     """
     state = relays.state()
     return render_template(
-        "keypad.html",
+        "dashboard.html",
         subject="dashboard",
-        actions=[
+        datetime=datetime.now().strftime("%d/%m/%y %H:%M:%S"),
+        relays=(
             (
                 relay.name,
                 "turn off" if id in state.keys() else "turn on",
@@ -53,10 +54,35 @@ def index():
                 state[id] if id in state.keys() else None,
             )
             for id, relay in relays.items()
-        ],
-        datetime=datetime.now().strftime("%d/%m/%y %H:%M:%S"),
+        ),
         times=(
             x["time"] for x in current_app.config["RUNNING_TIMES"]
             if x["time"] <= current_app.config["MAX_TIME"]
+        ),
+        sequences=(
+            (
+                sequitur.name,
+                "stop" if sequitur.running else "start",
+                "sequencer.stop" if sequitur.running else "sequencer.start",
+                {"sequence_id": sequitur.id},
+                True,
+                sequitur.running,
+                sequitur.current.name if sequitur.running else "not running",
+                sequitur.current.timer.remaining().total_seconds() if sequitur.running else "n/a",
+            )
+            for sequitur in sequences.values()
+        ),
+        schedules=(
+            (
+                schedule.name,
+                "deactivate" if schedule.active else "activate",
+                "schedule.deactivate" if schedule.active else "schedule.activate",
+                {"schedule_id": schedule.id},
+                True,
+                schedule.active,
+                schedule.jobs[0].sequitur.name if schedule.active else "not active",
+                schedule.jobs[0].upcoming() if schedule.active else "n/a",
+            )
+            for schedule in schedules.values()
         ),
     )
--- static/dashboard.js
+++ static/dashboard.js
@@ -1,8 +1,3 @@
-// document.getElementById("time").addEventListener("input", () => {
-//   setBubble(document.getElementById("time"),document.getElementById("time").nextElementSibling);
-// });
-// setBubble(document.getElementById("time"),document.getElementById("time").nextElementSibling);
-
 function setBubble(range, bubble) {
   const val = range.value;
   const min = range.min ? range.min : 0;
