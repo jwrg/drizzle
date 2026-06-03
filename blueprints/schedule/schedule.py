jwrg/drizzle

commit c8293eb4b12ca4ec3ab422002285d577f50981c7

Change Schedule form integer fields to a time field

blueprints/schedule/schedule.py
@@ -68,7 +68,13 @@ def index():
                             "weekdays": ", ".join(
                                 weekdays[weekday] for weekday in job.weekdays
                             ),
-                            "time": str(job.hour) + ":" + str(job.minute)
+                            "time": ':'.join([
+                                str(job.time.hour),
+                                str(job.time.minute),
+                            ]) if job.time.minute > 9 else ":0".join([
+                                str(job.time.hour),
+                                str(job.time.minute),
+                            ])
                         }
                         for ordinal, job in enumerate(schedule.jobs)
                     }
@@ -131,13 +137,15 @@ def edit(schedule_id):
     def validate_concurrency(form, field):
         if len(
             [
-                str(weekday.data) + str(d.hour.data) + str(d.minute.data)
-                for d in field.entries for weekday in d.weekdays
+                str(weekday) + str(d.time.data.hour) +
+                str(d.time.data.minute)
+                for d in field.entries for weekday in d.weekdays.data
             ]
         ) > len(
             {
-                str(weekday.data) + str(d.hour.data) + str(d.minute.data)
-                for d in field.entries for weekday in d.weekdays
+                str(weekday) + str(d.time.data.hour) +
+                str(d.time.data.minute)
+                for d in field.entries for weekday in d.weekdays.data
             }
         ):
             raise ValidationError(

util/schedule.py
@@ -25,13 +25,11 @@ class Job:
         self,
         sequence: Sequitur,
         weekdays: list[int],
-        hour: int,
-        minute: int
+        time: time
     ) -> None:
         self.sequence = sequence
         self.weekdays = weekdays
-        self.hour = hour
-        self.minute = minute
+        self.time = time
 
     def __eq__(self, obj: Job) -> bool:
         return self.upcoming() == obj.upcoming()
@@ -69,8 +67,8 @@ class Job:
         """
         thisweek = self.today() + timedelta(
             days=self.weekday() - (self.today().isoweekday() % 7),
-            hours=self.hour,
-            minutes=self.minute,
+            hours=self.time.hour,
+            minutes=self.time.minute,
         )
         return (
             thisweek if thisweek > datetime.now()
@@ -86,7 +84,7 @@ class Job:
         if today in self.weekdays:
             index = self.weekdays.index(today)
             if self.today() + timedelta(
-                hours=self.hour, minutes=self.minute
+                hours=self.time.hour, minutes=self.time.minute
             ) > datetime.now():
                 return today
             else:
@@ -236,8 +234,7 @@ class Scheduler(PersistentMapping):
                     Job(
                         sequences[job["sequence"]],
                         job["weekdays"],
-                        job["hour"],
-                        job["minute"]
+                        time(hour=job["hour"], minute=job["minute"])
                     )
                     for job in schedule["jobs"]
                 ),
@@ -256,8 +253,8 @@ class Scheduler(PersistentMapping):
                     {
                         "sequence": job.sequence.id,
                         "weekdays": job.weekdays,
-                        "hour": job.hour,
-                        "minute": job.minute
+                        "hour": job.time.hour,
+                        "minute": job.time.minute
                     }
                     for job in list(schedule.jobs)
                 ]

util/form.py
@@ -10,6 +10,7 @@ from wtforms import (
     IntegerField,
     StringField,
     SubmitField,
+    TimeField,
     validators
 )
 from wtforms.widgets import html_params
@@ -80,10 +81,7 @@ class SequiturForm(BasicForm):
 class FixtureForm(Form):
     sequence = SelectField(current_app.config["SEQUITUR_NAME"].capitalize())
     weekdays = SelectMultipleField('Weekdays', coerce=int)
-    hour = IntegerField('Hour', validators=[
-        validators.NumberRange(min=1, max=24)])
-    minute = IntegerField('Minute', validators=[
-        validators.NumberRange(min=0, max=59)])
+    time = TimeField('Time')
 
 
 class ScheduleForm(BasicForm):

commit f5f4e899165b046ff8a66f2bcada6ae0252f68cc

Fix error introduced by c8293eb4b12ca4ec3ab422002285d577f50981c7

blueprints/schedule/schedule.py
@@ -198,7 +198,7 @@ def edit(schedule_id):
             flash(form.errors, "error")
         else:
             while len(form.jobs.entries) > len(schedule.jobs):
-                schedule.jobs += [Job(None, 0, 0, 0)]
+                schedule.jobs += [Job(None, 0, time())]
             form.populate_obj(schedule)
             for job in schedule.jobs:
                 job.sequitur = sequences[job.sequitur]